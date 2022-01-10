a2a8446c0539dbc15f61a5456d19a7ce85a286be
Marcos Acosta <macosta1625@gmail.com>
2022-01-10 12:29:45 -0500

    Simplify sigmoid derivative to leverage sigmoid

--- nn/activations.py
+++ nn/activations.py
@@ -7,10 +7,10 @@ def tanh_prime(x):
   return 1 - np.tanh(x) ** 2
 
 def sigmoid(x):
-  return 1 / (1 + np.exp(-x + np.max(x)))
+  return 1 / (1 + np.exp(-x))
   
 def sigmoid_prime(x):
-  return np.exp(-x) / ((1 + np.exp(-x)) ** 2)
+  return sigmoid(x) * (1 - sigmoid(x))
 
 def softmax(x):
   e_x = np.exp(x - np.max(x))
